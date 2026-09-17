186cedb78e3ea22b7a8feb3dd17cbdb932136d3b
saksham9140 <sakshamyadav9140@gmail.com>
2026-09-17 21:31:20 +0530

    countVowel

---
+++ Lec-12 Strings/countVowel.java
@@ -0,0 +1,16 @@
+public class countVowel{
+    static int countVowel(String str){
+        int count=0;
+        for (int i = 0; i < str.length(); i++) {
+        char ch = str.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' ||ch == 'u' ){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        String str = "dgsfgwaaqeqer";
+        System.out.println(countVowel(str));
+    }
+}
